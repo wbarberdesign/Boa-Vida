d1ddef3d33ba5b1e8fa3b6adc64967f5793c7caa
Will Barber <wbarberdesign@gmail.com>
2021-08-24 10:14:47 +1200

    Up to date

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,9 @@
 // Put your application javascript here
+document.addEventListener("DOMContentLoaded", () => {
+    // ----------------- *
+    // LAZYLOAD IMAGES
+    // ----------------- *
+    var lazyLoadInstance = new LazyLoad({
+        elements_selector: "img"
+    });
+});
